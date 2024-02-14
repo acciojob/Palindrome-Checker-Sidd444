1cff5c777c3147fe14d620fdab9ebf8264b3538b
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2024-02-15 00:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+let l=0,r=str.length-1;
+	while(l<r) if(str[l++]!==str[r--]) return false;
+	return true;
 }
 module.exports = palindrome
